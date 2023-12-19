8822b6f2c5528bca34f3212e404049d66e8f9ac0
Liz Vergara <lizvcast@espol.edu.ec>
2023-12-19 13:29:19 -0500

    modal complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <header>
-            <h2 className="text-5xl font-bold text-blue-800 text-center py-8 mx-auto bg-yellow-300"> Pokemon Api Test
+            <h2 className="text-5xl font-bold text-black-800 text-center py-8 mx-auto bg-yellow-300"> Pokemon Api Test
             </h2>
         </header>
     )
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,58 +1,67 @@
-import React from 'react'
-import { useState, useEffect } from 'react'  
+import React, { useState, useEffect } from 'react';
 
-const Modal = ({setModal, modal, idPokemon}) => {
-    const [infoPokemon, setinfoPokemon] = useState([])
+const Modal = ({ setModal, modal, idPokemon }) => {
+  const [pokemonInfo, setPokemonInfo] = useState(null);
 
-    const poke = getPokemon(idPokemon)
-    console.log(poke)
+  useEffect(() => {
+    const getPokemonInfo = async () => {
+      try {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`);
+        const data = await res.json();
 
-    function getPokemon  (id)  {
-        const [pokemon, setPokemon] = useState([])
-        const getPokemonId = async () => {
-            const res = await fetch('https://pokeapi.co/api/v2/pokemon/'+ id)
-            const data = await res.json()
-            const pokemonTemp = {
-                name: data.name,
-                img: data.sprites.other.dream_world.front_default,
-                hability: data.abilities[0].ability.name,
-                types: data.types[0].type.name,
-            }
-            setPokemon(pokemonTemp)
-            return{
-              pokemon
-            }
-        }
-        useEffect(() => { getPokemonId() }, [])
-        return ( pokemon )
+        const pokemonData = {
+          name: data.name,
+          img: data.sprites.other.dream_world.front_default,
+          ability: data.abilities[0].ability.name,
+          type: data.types[0].type.name,
+          
+        };
 
-    }
-
-
-  
-    const handleOverlayClick = (e) => {
-      if (e.target.classList.contains('bg-black')) {
-        setModal(false);
-    }
+        setPokemonInfo(pokemonData);
+      } catch (error) {
+        console.error('Error fetching Pokemon data:', error);
+      }
     };
+
+    if (modal && idPokemon) {
+      getPokemonInfo();
+    }
+  }, [modal, idPokemon]);
+
+  const handleOverlayClick = (e) => {
+    if (e.target.classList.contains('bg-black')) {
+      setModal(false);
+    }
+  };
+
   return (
-    
     <>
-       {modal && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
-    onClick={handleOverlayClick}>
-            <div className="bg-white w-2/5 h-96 p-6 rounded-3xl shadow-md">
-                <h className="text-xl font-semibold mb-4 text-red-700">Informacion de{}</h>
-                <p className="text-gray-700">Contenido del modal...</p>
-                {/* <button className=" bg-blue-500 text-white px-4 py-2 rounded-md mt-auto">Cerrar</button> */}
-            </div>
+      {modal && (
+        <div
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-10"
+          onClick={handleOverlayClick}
+        >
+          <div className="bg-blue-100 w-2/3 md:w-1/2 lg:w-1/3 h-auto p-6 rounded-3xl shadow-md text-center z-10">
+            {pokemonInfo ? (
+              <>
+                <h2 className="text-xl font-semibold mb-4 text-red-700">Información de {pokemonInfo.name}</h2>
+                <img src={pokemonInfo.img} alt={pokemonInfo.name} className="mx-auto mb-4" />
+                <p className="text-gray-700">
+                  <strong>Nombre:</strong> {pokemonInfo.name}
+                  <br />
+                  <strong>Habilidad:</strong> {pokemonInfo.ability}
+                  <br />
+                  <strong>Tipo:</strong> {pokemonInfo.type}
+                </p>
+              </>
+            ) : (
+              <p className="text-gray-700">Cargando información del Pokémon...</p>
+            )}
+          </div>
         </div>
-        )}
-
-
+      )}
     </>
-    
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
